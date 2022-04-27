28375623d8e0a615073c3e1fb742e1be6a02c170
Yudi Calan Chan <101496814+YuDiCC@users.noreply.github.com>
2022-04-27 04:56:25 -0500

    Add files via upload

---
+++ FuncionSumar_dosNumerosE.py
@@ -0,0 +1,12 @@
+print(" ")
+print("----------------Funcion para sumar dos numeros enteros-------------------")
+print(" ")
+
+print("Ingresa tus valores: ")
+print(" ")
+
+x = int(input("x="))
+y = int(input("y="))
+
+print(" ")
+print("El resultado es: ", x + y)
